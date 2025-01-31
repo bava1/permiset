fd87b12773359014380680ea76d84488bcd4e65e
vadym.barvinok <vadym.barvinok@nev-dama.cz>
2025-01-31 10:07:23 +0100

    bava fix

--- apps/client/src/api/axiosClient.ts
+++ apps/client/src/api/axiosClient.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
-  // baseURL: process.env.RENDER_EXPRESS_API_URL || "https://permiset-express-latest.onrender.com",
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  baseURL: process.env.RENDER_EXPRESS_API_URL || "https://permiset-express-latest.onrender.com",
 });
 
 // ✅ Now we use `sessionStorage` so that the token is not lost when changing the language
